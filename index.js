5960e7555a98ceeda7527bf9f623a613e723b958
hemanshu1390 <ykasar138@gmail.com>
2024-11-07 11:21:51 +0530

    Add index.js

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,
+  {
+    connectTimeoutMS: 20000, // 20 seconds
+    socketTimeoutMS: 45000,  // 45 seconds
+  }
+)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log(error));
 
